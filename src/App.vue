617726332b36312d3b885a7195848d3f3f42d79b
Tia Rose <shortyshort789@aol.com>
2020-07-31 19:41:26 -0700

    got 4 buttons done

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,63 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <Button
+        type="<Button />"
+        color="#E0E0E0"
+        />
+    </div>
+    <div>
+      <Button
+        type="&:hover, &:focus"
+        color="#AEAEAE"
+        class="hoverOne"
+        />
+    </div>
+    <div>
+      <Button
+        type="<Button variant=”outline” />"
+        color="white"
+        class="outlineOne"
+        />
+    </div>
+    <div>
+      <Button
+        type="&:hover, &:focus"
+        color="white"
+        class="outlineOne"
+        id="hover"
+        />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Button from './components/Button.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Button
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    display: flex;
+    justify-content: space-evenly;
+  }
+
+  .hoverOne button:hover {
+      border: 2px solid #3D5AFE;
+  }
+
+  .outlineOne button {
+    color: #3D5AFE;
+    border: 1px solid #3D5AFE;
+    border-radius: 6px;
+  }
+  #hover button:hover {
+    border-width: 2px;
+  }
 </style>
---
+++ src/components/Button.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="button">
+    <p>{{type}}</p>
+    <button :style="{ backgroundColor: color }">{{buttonText}}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    type: String,
+    buttonText: {
+      type: String,
+      default: 'Default'
+    },
+    color: String
+  },
+  data: function() {
+    return {
+      // styleObject: {
+      //   fontSize: '13px',
+      //   position: 'absolute',
+      //   width: '81px',
+      //   height: '36px',
+      //   left: '319px',
+      //   top: '141px',
+      //   background: '#E0E0E0',
+      //   boxShadow: '0px 2px 3px rgba(51, 51, 51, 0.2)',
+      //   borderRadius: '6px',
+      // }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  button {
+    position: absolute;
+    width: 81px;
+    height: 36px;
+    box-shadow: 0px 2px 3px rgba(51, 51, 51, 0.2);
+    border-radius: 6px;
+
+
+  }
+</style>
